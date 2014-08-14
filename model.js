85db520d607a986667da3e19e90432fefee6469d
Ori <opleban@gmail.com>
2014-08-14 16:50:36 -0700

    adds column ids in html

--- model.js
+++ model.js
@@ -14,10 +14,10 @@ function Board (height,width) {
   // inner_array_of_blanks.map(function(val,index,array) {array[index] = new Square();});
   // this.cells = outer_array_of_rows.map(function() {return inner_array_of_square_objs});
   var inner_array_of_square_objs = new Array(width);
-  for (i=0;i<inner_array_of_blanks.length;i++) {
+  for (var i=0;i<inner_array_of_blanks.length;i++) {
     inner_array_of_square_objs[i] = new Square();
   }
-  for (i=0;i<outer_array_of_rows.length;i++) {
+  for (var i=0;i<outer_array_of_rows.length;i++) {
     outer_array_of_rows[i] = inner_array_of_square_objs;
   }
   this.cells = outer_array_of_rows;
@@ -25,14 +25,14 @@ function Board (height,width) {
 
 Board.prototype = {
   addChip: function (column) {
-    for (i=(this.height-1);i>0;i--) {
-      if this.cells[i][column].color == ""
+    for (var i=(this.height-1);i>0;i--) {
+      if (this.cells[i][column].color == "")
         this.cells[i][column].color = this.currentColor
     }
 
   }
   findOpenSpace: function (column){
-    for (i=(this.height-1);i>0;i--) {
+    for (var i=(this.height-1);i>0;i--) {
       this.cells[i][column]
     }
   }
