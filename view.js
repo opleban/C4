13d0630612a2e6c674ca825a144e2d9bdadfa920
Ori <opleban@gmail.com>
2014-08-14 18:11:37 -0700

    adds simple view methods

--- view.js
+++ view.js
@@ -1,14 +1,23 @@
 View = {
 
   updateBoard: function(square_id, color){
-
+    $('#'+square_id).addClass(color);
   },
 
-  changeColor: function(){},
+  displayChangeColor: function(color){
+    alert("It's " + color + "'s turn!");
+  },
 
-  dislay_win: function(color){}
+  dislay_win: function(color){
+    alert(color + "wins!")
+  },
 
   buttons: function(){
+    return $(button);
+  },
+
+  displayColumnFull: function(){
+    alert("Illegal move: that column is already full!")
 
   }
 }
